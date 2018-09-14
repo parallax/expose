f99ad9eff0ff6d9f5124b1a0f65e48812503f678
Brad Cornes <brad@parall.ax>
2018-09-14 12:55:57 +0100

    [expose-react] update highlighter styles

--- packages/expose-react/src/highlighter.js
+++ packages/expose-react/src/highlighter.js
@@ -29,7 +29,7 @@ export default class Highlighter extends Component {
       <div
         style={{
           position: 'absolute',
-          border: '1px solid red',
+          border: '1px dashed #8360d6',
           pointerEvents: 'none',
           ...this.state.styles
         }}
@@ -38,6 +38,7 @@ export default class Highlighter extends Component {
           <button
             type="button"
             style={{
+              appearance: 'none',
               position: 'absolute',
               bottom: -16,
               right: -16,
@@ -52,26 +53,46 @@ export default class Highlighter extends Component {
           />
         )}
         {this.state.editableProps && (
-          <button
-            class="expose-show-editable-popout"
-            type="button"
+          <div
             style={{
               position: 'absolute',
               top: -1,
               left: -32,
-              width: 32,
-              height: 32,
-              pointerEvents: 'auto'
+              pointerEvents: 'auto',
+              boxShadow: '0px 4px 9px 0px rgba(0,0,0,0.39)',
+              display: 'flex'
             }}
-            onClick={() => {
-              window.parent &&
-                window.parent.Expose.showEditableOptions({
-                  location: this.state.location,
-                  options: this.state.editableProps,
-                  stateContainer: this.state.editableStateContainer
-                })
-            }}
-          />
+          >
+            <button
+              class="expose-show-editable-popout"
+              type="button"
+              style={{
+                appearance: 'none',
+                width: 32,
+                height: 32,
+                background: '#453f56',
+                border: 0,
+                borderRadius: 0,
+                padding: 0,
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                cursor: 'pointer'
+              }}
+              onClick={() => {
+                window.parent &&
+                  window.parent.Expose.showEditableOptions({
+                    location: this.state.location,
+                    options: this.state.editableProps,
+                    stateContainer: this.state.editableStateContainer
+                  })
+              }}
+            >
+              <svg width={16} height={16} viewBox="0 0 20 20" fill="#fff">
+                <path d="M15.95 10.78c.03-.25.05-.51.05-.78s-.02-.53-.06-.78l1.69-1.32c.15-.12.19-.34.1-.51l-1.6-2.77c-.1-.18-.31-.24-.49-.18l-1.99.8c-.42-.32-.86-.58-1.35-.78L12 2.34c-.03-.2-.2-.34-.4-.34H8.4c-.2 0-.36.14-.39.34l-.3 2.12c-.49.2-.94.47-1.35.78l-1.99-.8c-.18-.07-.39 0-.49.18l-1.6 2.77c-.1.18-.06.39.1.51l1.69 1.32c-.04.25-.07.52-.07.78s.02.53.06.78L2.37 12.1c-.15.12-.19.34-.1.51l1.6 2.77c.1.18.31.24.49.18l1.99-.8c.42.32.86.58 1.35.78l.3 2.12c.04.2.2.34.4.34h3.2c.2 0 .37-.14.39-.34l.3-2.12c.49-.2.94-.47 1.35-.78l1.99.8c.18.07.39 0 .49-.18l1.6-2.77c.1-.18.06-.39-.1-.51l-1.67-1.32zM10 13c-1.65 0-3-1.35-3-3s1.35-3 3-3 3 1.35 3 3-1.35 3-3 3z" />
+              </svg>
+            </button>
+          </div>
         )}
       </div>
     ) : null
