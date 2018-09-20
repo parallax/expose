8b5fa27dd944493dfb8615683c6f20afe873602d
Brad Cornes <brad@parall.ax>
2018-09-20 16:36:41 +0100

    [expose-react] fix location join (was discarding zeros)

--- packages/expose-react/src/util.js
+++ packages/expose-react/src/util.js
@@ -4,7 +4,9 @@ let root = typeof window === 'undefined' ? global : window
 let isAdmin = dlv(root, 'Expose.isAdmin', false)
 
 function joinLocation(...locations) {
-  return locations.filter(Boolean).join('.')
+  return locations
+    .filter(x => x !== null && x !== '' && typeof x !== 'undefined') // 0 (zero) is valid
+    .join('.')
 }
 
 export { root, isAdmin, joinLocation }
